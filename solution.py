579b95e102856d2240fcd2f1fd7932bf134eea3f
Daniil <50438498+ctbip@users.noreply.github.com>
2023-04-13 19:52:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 784892881 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x, y=None) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    
+    if y==None:
+        t_stat, p_val = ttest_ind(x, x)
+    else:
+        t_stat, p_val = ttest_ind(x, y)
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    significance_level = 0.06
+    
+    if p_val < significance_level:
+        return True         # print("Reject null hypothesis: there is a significant difference.")
+    else:
+        return False        # print("Fail to reject null hypothesis: there is no significant difference.")
